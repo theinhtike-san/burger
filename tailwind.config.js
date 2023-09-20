7593624df762d5516fdadc3ed147df4039b321bb
theinhtike-san <theinhtikesan.personal@gmail.com>
2023-09-20 17:56:51 +0630

    config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/*.{html,js}",
+          "./index.html",
     "./node_modules/flowbite/**/*.js"],
   theme: {
     fontFamily: {
